21923065c948b2a074c981da7dce466853da3f84
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-04-02 09:10:22 -0400

    update

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,24 @@
 
+import {Routes, Route} from "react-router-dom"
 
-function App() {
+import React from 'react'
+import Login from "./page/Login"
+import Home from "./page/Home"
+
+const App = () => {
   return (
-    <div >
-      <h2>hi</h2>
+    <div>
+ <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/*" element={<Home />} />
+
+    </Routes>
+
     </div>
-  );
+  )
+}
+
+export default App
