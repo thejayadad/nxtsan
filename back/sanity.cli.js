1e8b1d9051c87f8afd9251ee2781ecb5e5448c4a
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-04-02 00:00:59 -0400

    update

---
+++ back/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'c87nclcs',
+    dataset: 'production'
+  }
+})
---
+++ back/schemas/index.js
@@ -0,0 +1,7 @@
+import user from "./user.js"
+import comment from "./comment.js"
+import postedBy from "./postedBy.js"
+import post from "./post.js"
+import save from "./save.js"
+
+export const schemaTypes = [user, comment, postedBy, post, save]
