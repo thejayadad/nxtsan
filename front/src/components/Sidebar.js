9c31a94c5103fb582943319bd5041df9a5ae0f99
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-04-02 12:45:12 -0400

    update

--- front/src/components/Sidebar.js
+++ front/src/components/Sidebar.js
@@ -1,6 +1,7 @@
 
 import React from 'react'
 import { NavLink, Link } from 'react-router-dom';
+import { categories } from '../utils/data';
 
 
 
@@ -21,9 +22,42 @@ const Sidebar = ({ closeToggle, user }) => {
         >
           <img src="" alt="logo" className="w-full" />
         </Link>
-        <div className="flex flex-col gap-5"></div>
-    </div>
+        <div className="flex flex-col gap-5">
+
+        <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? isActiveStyle : isNotActiveStyle)}
+            onClick={handleCloseSidebar}
+          >
+            Home
+          </NavLink>
+          <h3 className="mt-2 px-5 text-base 2xl:text-xl">Discover cateogries</h3>
+          {categories.slice(0, categories.length - 1).map((category) => (
+            <NavLink
+              to={`/category/${category.name}`}
+              className={({ isActive }) => (isActive ? isActiveStyle : isNotActiveStyle)}
+              onClick={handleCloseSidebar}
+              key={category.name}
+            >
+              <img src={category.image} className="w-8 h-8 rounded-full shadow-sm" alt="catImage" />
+              {category.name}
+            </NavLink>
+          ))}
         </div>
+      </div>
+      {user && (
+        <Link
+          to={`user-profile/${user._id}`}
+          className="flex my-5 mb-3 gap-2 p-2 items-center bg-white rounded-lg shadow-lg mx-3"
+          onClick={handleCloseSidebar}
+        >
+          <img src={user.image} className="w-10 h-10 rounded-full" alt="user-profile" />
+          <p>{user.userName}</p>
+       </Link>
+      )}
+        </div>
+
+
   )
 }
 
